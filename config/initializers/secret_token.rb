a2537361c8dd8035ca5b29fd89c1182ff011a608
Ran Tao <ran@ephexi.com>
2012-08-06 21:21:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shipshop::Application.config.secret_token = '9eb6d33289281245838e65d7fbe3e27afa19916077689ed2b056036b8cb7366a9d52fe5a25599e4054525c89109993d337d9572350c8261413c3e149d44fd356'
